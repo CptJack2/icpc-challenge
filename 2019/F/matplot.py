a0c574218e925dc0dcab83a383cecb7cd5d90211
leninchen <leninchen@tencent.com>
2021-12-24 17:24:19 +0800

    plot

--- 2019/F/matplot.py
+++ 2019/F/matplot.py
@@ -1,6 +1,7 @@
 import pdb
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 l=10
 r=20
@@ -21,14 +22,18 @@ topo_sorted=[
 plt.subplot(1,2,1)
 
 for i,p in enumerate(ps):
-    plt.plot([p[0],p[2]],[p[1],ps[3]],linestyle="solid",color="black")
+    plt.plot([p[0],p[2]],[p[1],p[3]],linestyle="solid",color="black")
     plt.text(p[0],p[1],str(i+1))
 plt.plot([l,r],[0,0],linestyle="solid",color="green")
 
 plt.subplot(1,2,2)
+plt.plot([l,r],[0,0],linestyle="solid",color="green")
 for i,arr in enumerate(topo_sorted):
     for j in arr:
-        plt.arrow(ps[j-1][0],i,ps[j-1][2]-ps[j-1][0],0)
+        x1=ps[j-1][0]
+        x2=ps[j-1][2]
+        plt.arrow(x2,i+1,x1-x2,0,head_width=0.1,head_length=1.5,length_includes_head=True)
+        plt.text(x2,i+1,str(j))
 
 
 plt.show()
