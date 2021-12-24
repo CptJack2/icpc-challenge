1510cc3922449b63a41ca5bfe6a16bf9154fac1e
leninchen <leninchen@tencent.com>
2021-12-24 16:52:57 +0800

    tmp

--- 2019/F/matplot.py
+++ 2019/F/matplot.py
@@ -1,19 +1,35 @@
+import pdb
+
 import matplotlib.pyplot as plt
 
 l=10
 r=20
-ps=(
-32, 50, 12, 60,
-30, 60, 8, 70,
-25, 70, 0, 80,
-15, 30, 28, 40,
-5, 20, 14, 25,
-)
+ps=[
+    [32, 50, 12, 60,],
+    [30, 60, 8, 70,],
+    [25, 70, 0, 80,],
+    [15, 30, 28, 40,],
+    [5, 20, 14, 25,],
+]
+topo_sorted=[
+    [4, 5],
+    [1],
+    [2],
+    [3],
+]
 
-for i in range(0,len(ps),4):
-    plt.plot([ps[i],ps[i+2]],[ps[i+1],ps[i+3]],linestyle="solid",color="black")
-    plt.text(ps[i],ps[i+1],str(int(i/4+1)))
+plt.subplot(1,2,1)
+
+for i,p in enumerate(ps):
+    plt.plot([p[0],p[2]],[p[1],ps[3]],linestyle="solid",color="black")
+    plt.text(p[0],p[1],str(i+1))
 plt.plot([l,r],[0,0],linestyle="solid",color="green")
+
+plt.subplot(1,2,2)
+for i,arr in enumerate(topo_sorted):
+    for j in arr:
+        plt.arrow(ps[j-1][0],i,ps[j-1][2]-ps[j-1][0],0)
+
 
 plt.show()
 
