97f1784a2b89f37ec8348922104a94de22ebcd62
leninchen <leninchen@tencent.com>
2022-01-07 15:25:52 +0800

    dp delta tmp

--- 2019/F/solution.py
+++ 2019/F/solution.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import bisect
 
 def get_dep(N,allX,data):
     #各个tarp之间的上下依赖关系, 内层数组元素是tuple，0为依赖发生的x坐标，1为依赖的index
@@ -90,8 +91,79 @@ def cal_y(x,x1,y1,x2,y2):
     return float(y1-y2)/(x1-x2)*(x-x1)+y1
 
 def find_solution_dp():
-    minx,maxx=allX[0].x,allX[-1].x
     inf=999999999999
+    def find_ge(a, x):
+        'Find leftmost item greater than or equal to x'
+        i = bisect.bisect_left(a, x)
+        if i != len(a):
+            return a[i]
+        raise ValueError
+    def find_le(a, x):
+        'Find rightmost value less than or equal to x'
+        i = bisect.bisect_right(a, x)
+        if i:
+            return a[i-1]
+        raise ValueError
+    def find_gt(a, x):
+        'Find leftmost value greater than x'
+        i = bisect.bisect_right(a, x)
+        if i != len(a):
+            return a[i]
+        #todo what if x greater than all value in a
+        raise ValueError
+    class dp_ele:
+        def __init__(self,x,delta):
+            self.x=x
+            self.delta=delta
+        def __lt__(self, other):
+            return self.x < other.x
+    class dp_solver:
+        def __init__(self,L,R):
+            self.deltas=[dp_ele(L,-inf),dp_ele(R+1,inf)]
+        def roll(self, start, end):
+            if start<end:
+                i=find_gt(self.deltas, dp_ele(start, 0))
+                start=self.deltas[i].x
+                while start<=end:
+                    if self.deltas[i].delta>0:
+                        if i<=len(self.deltas)-2:
+                            if self.deltas[i]==inf or self.deltas[i]==-inf:
+                                self.deltas[i+1].delta=self.deltas[i].delta
+                            else:
+                                self.deltas[i+1].delta+=self.deltas[i]
+                            self.deltas[i].pop(i)
+                    else:
+                        i+=1
+                    start=self.deltas[i].x
+            else:
+                i=find_le(self.deltas, dp_ele(start, 0))
+                start=self.deltas[i].x
+                while start>=end:
+                    if self.deltas[i].delta<0:
+                        if i>=1:
+                            if self.deltas[i]==inf or self.deltas[i]==-inf:
+                                self.deltas[i-1].delta=self.deltas[i]
+                            else:
+                                self.deltas[i-1].delta+=self.deltas[i]
+                            self.deltas[i].pop(i)
+                    i-=1
+                    start=self.deltas[i].x
+        def add(self,start,end):
+            if start<end:
+                i=find_ge(self.deltas, dp_ele(start, 0))
+                if self.deltas[i].x==start:
+                    self.deltas[i]+=1
+                else:
+                    self.deltas.insert(i,dp_ele(start,1))
+                i=find_gt(self.deltas, dp_ele(end, 0))
+                if self.deltas[i].x==end:
+                    self.deltas[i]+=1
+                else:
+                    self.deltas.insert(i,dp_ele(start,1))
+            else:
+                i
+
+    minx,maxx=allX[0].x,allX[-1].x
     dp=[inf for i in range(minx,maxx+1)]
     dp[L:R+1]=[0 for i in range(L,R+1)]
     for tarp_index in reversed(sorted):
