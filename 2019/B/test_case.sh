3dacc1a64fb5fe6165b39d686be1ee02ef60e88a
CptJack <844501812@qq.com>
2022-01-20 13:07:27 +0800

    2019 B

---
+++ 2019/B/test_case.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+input=$(ls ../icpc2019data/B-beautifulbridges/*.in)
+count=0
+for inf in $input;do
+#  if(( $count >= 2 ));then exit; fi
+  echo $inf
+  filename=$(basename -- "$inf")
+  extension="${filename##*.}"
+  ans_filename="${filename%.*}"".ans"
+  ans=$(cat $inf |./main)
+  correct=$(cat ../icpc2019data/B-beautifulbridges/$ans_filename)
+  if [ "$ans" != "$correct" ];then
+    echo "failed"
+    echo "correct " correct
+    echo "ans " $ans
+  else
+    echo "succ"
+  fi
+  count=$(( $count + 1 ))
+done
