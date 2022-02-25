bc58e3ac533478c43da73c70da440b1112bffdb8
CptJack <844501812@qq.com>
2022-02-25 19:35:50 +0800

    J

---
+++ 2019/J/ans_commented.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int P, H;
+	while (cin >> P >> H) {
+		vector<vector<int>> S(P, vector<int>(H));
+		vector<int64_t> tot(P);
+		for (int i = 0; i < P; i++) {
+			for (int j = 0; j < H; j++) {
+				cin >> S[i][j];
+				tot[i] += S[i][j];
+			}
+			S[i].push_back(0);//设置一个最小的地板，在26行的循环递增条件用
+			sort(S[i].begin(), S[i].end(), greater<int>());
+		}
+
+		for (int i = 0; i < P; i++) {
+			vector<pair<int, int>> events;//pair中第一个int为取的lim,第二个表示取此lim时,排名需要如何变化
+			int cur = 0;
+			for (int j = 0; j < P; j++) {
+				int64_t itot = tot[i], jtot = tot[j], lim = 1000000000;
+				if (jtot <= itot) cur++;
+				for (int ih = 0, jh = 0; ih < H || jh < H; S[i][ih] > S[j][jh] ? ih++ : jh++) {
+					bool old = (jtot <= itot);//这里的条件比较巧妙，具体看下面
+					int v = max(S[i][ih], S[j][jh]);//不断取ij的scores中较大者为lim,如果有重复,当两个都到达小值时才会进入下一lim
+					itot -= (lim-v) * ih; jtot -= (lim-v) * jh;//对两者总score进行裁减
+					lim = v;
+					if (!old && jtot <= itot) {
+						/*排名要+1，在前一lim需要jtot>itot（=不行，因为排名算的是小于等于自己分数的个数），当前lim jtot <= itot
+						 *直线过（lim，tot）点，斜率是h，分别列出ij两直线的方程式，求出交点横坐标就是lim+(itot-jtot)/(jh-ih), 同时计算出的是double,需要向下取整(转int已经执行了)
+						 *判断条件相当于if(orig_jtot>orig_itot && (jtot<itot || jtot==itot))*/
+						events.emplace_back(lim+(itot-jtot)/(jh-ih), 1);
+					} else if (old && jtot > itot) {
+						/*排名要-1，在前一lim需要jtot<=itot，在当前lim jtot>itot，并且如果交点是整数,-1的效果要计入前一个点，所以会有jtot-itot-1
+						 *判断条件相当于if((orig_jtot>orig_itot || orig_jtot==orig_itot) && jtot>itot)*/
+						events.emplace_back(lim+(jtot-itot-1)/(ih-jh), -1);
+					}
+				}
+			}
+
+			sort(events.begin(), events.end(), greater<pair<int, int>>());
+			int ret = cur;
+			for (auto const& e : events) { cur += e.second; ret = min(ret, cur); }
+			cout << ret << endl;
+		}
+	}
+}
--- 2019/J/main.cpp
+++ 2019/J/main.cpp
@@ -1,50 +1,62 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include "bits/stdc++.h"
 using namespace std;
 
 int main() {
 	int P, H;
-	while (cin >> P >> H) {
-		vector<vector<int>> S(P, vector<int>(H));
-		vector<int64_t> tot(P);
-		for (int i = 0; i < P; i++) {
-			for (int j = 0; j < H; j++) {
-				cin >> S[i][j];
-				tot[i] += S[i][j];
-			}
-			S[i].push_back(0);//设置一个最小的地板，在26行的循环递增条件用
-			sort(S[i].begin(), S[i].end(), greater<int>());
+	vector<vector<int>> scores(P, vector<int>(H));
+	vector<int> sum(P,0);
+	for (int i = 0; i < P; i++) {
+		for (int j = 0; j < H; j++) {
+			cin >> scores[i][j];
+			sum[i] += scores[i][j];
 		}
+		sort(scores[i].begin(),scores[i].end(),greater<int>());
+	}
 
-		for (int i = 0; i < P; i++) {
-			vector<pair<int, int>> events;//pair中第一个int为取的lim,第二个表示取此lim时,排名需要如何变化
-			int cur = 0;
-			for (int j = 0; j < P; j++) {
-				int64_t itot = tot[i], jtot = tot[j], lim = 1000000000;
-				if (jtot <= itot) cur++;
-				for (int ih = 0, jh = 0; ih < H || jh < H; S[i][ih] > S[j][jh] ? ih++ : jh++) {
-					bool old = (jtot <= itot);//这里的条件比较巧妙，具体看下面
-					int v = max(S[i][ih], S[j][jh]);//不断取ij的scores中较大者为lim,如果有重复,当两个都到达小值时才会进入下一lim
-					itot -= (lim-v) * ih; jtot -= (lim-v) * jh;//对两者总score进行裁减
-					lim = v;
-					if (!old && jtot <= itot) {
-						/*排名要+1，在前一lim需要jtot>itot（=不行，因为排名算的是小于等于自己分数的个数），当前lim jtot <= itot
-						 *直线过（lim，tot）点，斜率是h，分别列出ij两直线的方程式，求出交点横坐标就是lim+(itot-jtot)/(jh-ih)
-						 *判断条件相当于if(orig_jtot>orig_itot && (jtot<itot || jtot==itot))*/
-						events.emplace_back(lim+(itot-jtot)/(jh-ih), 1);
-					} else if (old && jtot > itot) {
-						/*排名要-1，在前一lim需要jtot<=itot，在当前lim jtot>itot，并且-1的效果要计入交点的向下取整的点，所以会有jtot-itot-1
-						 *判断条件相当于if((orig_jtot>orig_itot || orig_jtot==orig_itot) && jtot>itot)*/
-						events.emplace_back(lim+(jtot-itot-1)/(ih-jh), -1);
-					}
+	for (int i = 0; i < P; i++) {
+		int rank = 1;//自己的一个
+		//pair中第一个int为取的lim,第二个表示取此lim时,排名需要如何变化
+		vector<pair<int, int>> rankChange;
+		for (int j = 0; j < P; j++) {
+			if (j == i)
+				continue;
+			int isum = sum[i], jsum = sum[j];
+			if (jsum <= isum)
+				++rank;
+			//以i号玩家和j号玩家的分数作为limit
+			set<int, greater<int>> limits;
+			for (auto l:scores[i])limits.insert(l);
+			for (auto l:scores[j])limits.insert(l);
+			limits.insert(999999999);//加一个无限制
+			int iLimitedHoles = 0, jLimitedHoles = 0;
+			for (auto it = limits.begin(); it != prev(limits.end()); ++it) {
+				bool jLessEqual = (jsum <= isum);
+				int lim = *next(it), prevLim = *it;
+				isum -= (prevLim - lim) * iLimitedHoles;
+				jsum -= (prevLim - lim) * jLimitedHoles;
+				while (lim < scores[i][iLimitedHoles])++iLimitedHoles;
+				while (lim < scores[j][jLimitedHoles])++jLimitedHoles;
+				//排名要+1，在前一lim需要jsum>isum（=不行，因为排名算的是小于等于自己分数的个数），当前lim jsum <= isum
+				//排名要-1，在前一lim需要jsum<=isum，在当前lim jsum>isum
+				if (!jLessEqual && jsum <= isum || jLessEqual && jsum > isum) {
+					//每一条分段直线过（lim，sum）点，斜率是LimitedHoles，分别列出ij两直线的方程式，求出交点横坐标就是lim+(isum-jsum)/(jLimitedHoles-iLimitedHoles)
+					double intersectionPoint = lim + double(isum - jsum) / (jLimitedHoles - iLimitedHoles);
+					//因为lim都是整数,在交点的向下取整位置记入更新
+					int updatePoint = floor(intersectionPoint);
+					//对于-1的效果,如果交点是整数,需要计入前一个点
+					if (jLessEqual && jsum > isum && int(intersectionPoint) == intersectionPoint)
+						--updatePoint;
+					rankChange.emplace_back(updatePoint, !jLessEqual ? 1 : -1);
 				}
 			}
-
-			sort(events.begin(), events.end(), greater<pair<int, int>>());
-			int ret = cur;
-			for (auto const& e : events) { cur += e.second; ret = min(ret, cur); }
-			cout << ret << endl;
 		}
+		//根据更新点大小从大到小排序rankChange
+		sort(rankChange.begin(), rankChange.end(), greater<pair<int,int>>());
+		int minRank = rank;
+		for (auto const &r : rankChange) {
+			rank += r.second;
+			minRank = min(minRank, rank);
+		}
+		cout << minRank<< endl;
 	}
 }
