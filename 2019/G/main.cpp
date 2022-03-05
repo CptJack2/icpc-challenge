4c3b63b823600c3207d9f542572eff9977a463d7
CptJack <844501812@qq.com>
2022-03-05 21:34:04 +0800

    binary lifting

--- 2019/G/main.cpp
+++ 2019/G/main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 struct TrieNode{
 	char c;
+	int index;
 	TrieNode* parent;
+	vector<TrieNode*> children;
+	vector<TrieNode*> binayAncestors;//倍增的祖先
 };
 
 int n,k;
@@ -16,31 +19,43 @@ int main(){
 		int index;
 		cin>>c>>index;
 		nodes[i].c=c;
+		nodes[i].index=i;
 		nodes[i].parent=&nodes[index];
+		nodes[i].parent->children.push_back(&nodes[i]);
 		usedAlpha.insert(c);
 	}
-	vector<int> alphaRanking(26);
-	int count=0;
-	for(char c:usedAlpha)
-		alphaRanking[c-'A']=count++;
-	vector<int> sortedNodes(n);
-	for (int i = 0; i < n; ++i)
-		sortedNodes[i]=i+1;
-	auto cmp=[&](const int& node1, const int& node2){
-		TrieNode* p1=&nodes[node1], *p2=&nodes[node2];
-		while(p1!= nullptr && p2!= nullptr){
-			if(p1->c < p2->c)
-				return true;
-			else if(p1->c > p2->c)
-				return false;
-			else{
-				p1=p1->parent;
-				p2=p2->parent;
-			}
-		}
-		//exactly two same name
-		return true;
+	//寻找倍增祖先
+	vector<TrieNode*> dfsStack;
+	int trieHeight=0;//字典树高度
+	function<void(TrieNode*)> dfs=[&](TrieNode* node){
+		for(int lv=1;lv<dfsStack.size();lv*=2)
+			node->binayAncestors.push_back(dfsStack[dfsStack.size()-lv]);
+		dfsStack.push_back(node);
+		if(dfsStack.size() > trieHeight)
+			trieHeight=dfsStack.size();
+		for(auto ch:node->children)
+			dfs(ch);
+		dfsStack.pop_back();
 	};
-	sort(sortedNodes.begin(),sortedNodes.end(),cmp);
-	lower_bound(sortedNodes.begin(),sortedNodes.end(),)
+	dfs(&nodes[1]);
+	//统计第一个字母的排序
+	vector<int> rank(n+1);//queen name的前2^k个字母的排序
+	for (int i = 0; i <= n; ++i)
+		rank[i]=nodes[i].c-'A'+1;
+	struct rankKey{
+		int rankFront;//queen name的前缀前半段rank
+		int rankBack;//queen name的前缀后半段rank
+	};
+	for(int power=0;int dist=pow(2,power)<=trieHeight;power++){
+		auto cmp=[&](const rankKey& p1,const rankKey& p2){
+			return p1.rankFront<p2.rankFront || p1.rankFront==p2.rankFront && p1.rankBack<p2.rankBack;
+		};
+		map<rankKey,int,decltype(cmp)>distinctRankKeys(cmp);//用map对不同的rankKey排序
+		for(int i=1;i<=n;++i)
+			distinctRankKeys[rankKey{rank[i], i+dist<n? rank[i+dist] : 0}]=distinctRankKeys.size()+1;
+		//更新rank
+		for (int i = 1; i <= n; ++i)
+			rank[i]=distinctRankKeys[rankKey{rank[i], i+dist<n? rank[i+dist] : 0}];
+	}
+
 }
