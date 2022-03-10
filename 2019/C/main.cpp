98794339e702e5cec04b4a473d63c2b04f0562e8
CptJack <844501812@qq.com>
2022-03-10 11:21:41 +0800

    input

--- 2019/C/main.cpp
+++ 2019/C/main.cpp
@@ -9,19 +9,40 @@ enum moveType{
 };
 typedef pair<int,int> pii;
 struct Move{
-	pii src;
-	pii dest;
+	int src;
+	int dest;
 	moveType type;
-	vector<pii> midway;
+	vector<int> midway;
 };
 vector<Move> blackMoves, whiteMoves;
 int main(){
+	//read input
 	cin>>firstMove>>moveNum;
 	vector<Move>& move1=firstMove=='W'?whiteMoves:blackMoves,
-		move2=firstMove=='W'?blackMoves:whiteMoves;
+		&move2=firstMove=='W'?blackMoves:whiteMoves;
 	for (int i = 0; i < moveNum; ++i) {
 		Move theMove;
 		cin>>theMove.src;
 		char c;
+		cin>>c;
+		if(c=='-'){
+			theMove.type=moveType::move;
+			cin>>theMove.dest;
+		} else{
+			theMove.type=moveType::jump;
+			while(1){
+				int t;
+				cin >> t;
+				theMove.midway.push_back(t);
+				if(cin.peek()!='x')
+					break;
+				else
+					cin>>c;
+			}
+			theMove.dest=theMove.midway.back();
+			theMove.midway.pop_back();
+		}
+		i%2? move2.push_back(theMove): move1.push_back(theMove);
 	}
+
 }
