5a9cc4a8158b51de59a84e47021fc2912b00e032
CptJack <844501812@qq.com>
2022-03-13 20:15:29 +0800

    K

--- 2019/K/main.cpp
+++ 2019/K/main.cpp
@@ -16,24 +16,33 @@ long double k[V+5];
 int vis[V+5][V+5];
 
 inline void Solve(const int & t)/*余数为t时*/ {
-	register int i,j;
+ 	int i,j;
 	for(i=1;i<=V;++i) 
 		k[i]=1;
 	memset(vis,0,sizeof(vis));//初始化
-	register int x,w;
+ 	int x,w;
 	long double o=1;
-	for(++f[0],i=1;i<=n&&o>1e-12;f[i++]+=(o*=k[w]))
-		for(o/=k[w=T(p[i]/gcd(P,p[i]))],j=0;j^w;++j)
-			tg[i][x=(j*P+t)%p[i]]&&!vis[w][j]&&(k[w]-=1.0/w,vis[w][j]=1);//枚举时间jP+t
+	for(++f[0],i=1; i<=n&&o>1e-12 ;f[i++]+=(o*=k[w]))
+		for(o/=k[w=T(p[i]/gcd(P,p[i]))],j=0;j^w;++j){
+			tg[i][x=(j*P+t)%p[i]] &&
+				!vis[w][j] &&
+				(k[w]-=1.0/w,vis[w][j]=1);//枚举时间jP+t
+		}
 }
 int main(){
-	register int i,j,x,y;
-	for(scanf("%d",&n),i=1;i<=n;++i)
-		for(scanf("%d%d%d",&x,&y,p+i),x%=(p[i]+=y),j=0;j^y;++j)
+	int x,r;
+	cin>>n;
+	for(int i=1;i<=n;++i) {
+		cin>>x>>r>>p[i];
+		p[i]+=r;
+		x%=p[i];
+		for (int j = 0; j < r; ++j) {
 			tg[i][(j-x+p[i])%p[i]]=1;//给红灯对应余数打标记
-	for(i=0;i!=P;++i)
+		}
+	}
+	for(int i=0;i!=P;++i)
 		Solve(i);
-	for(i=1;i<=n+1;++i)
+	for(int i=1;i<=n+1;++i)
 		printf("%.12Lf\n",(f[i-1]-f[i])/P);
 	return 0;//枚举模P余数，最终答案要除以P
 }
---
+++ 2019/K/test_case.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ $1 == "case" ]];then
+  correct=$(cat ../icpc2019data/K-trafficblights/$2.ans)
+  ans=$(cat ../icpc2019data/K-trafficblights/$2.in |./main)
+  if [ "$ans" != "$correct" ];then
+    echo "failed"
+  else
+    echo "succ"
+  fi
+  exit
+fi
+
+if [[ $1 == "prev" ]]; then
+  cat ../icpc2019data/K-trafficblights/my_ans
+  exit
+fi
+
+input=$(ls ../icpc2019data/K-trafficblights/*.in)
+count=0
+for inf in $input;do
+#  if(( $count >= 2 ));then exit; fi
+  echo $inf
+  filename=$(basename -- "$inf")
+  extension="${filename##*.}"
+  ans_filename="${filename%.*}"".ans"
+  ans=$(cat $inf |./main)
+  echo "$ans" > ../icpc2019data/K-trafficblights/my_ans
+  correct=$(cat ../icpc2019data/K-trafficblights/$ans_filename)
+  if [ "$ans" != "$correct" ];then
+    echo "failed"
+#    echo "correct " $correct
+#    echo "ans " $ans
+    diff ../icpc2019data/K-trafficblights/$ans_filename ../icpc2019data/K-trafficblights/my_ans
+    exit
+  else
+    echo "succ"
+  fi
+  count=$(( $count + 1 ))
+done
