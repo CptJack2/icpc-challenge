9abab3d2e13ff29ba0537d5bf7e84977ab3ad978
CptJack <844501812@qq.com>
2022-03-20 22:14:20 +0800

    G ac automaton

--- 2019/G/main_ac_automaton.cpp
+++ 2019/G/main_ac_automaton.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 struct TrieNode{
 	char ch;
+	TrieNode* parent;
 	vector<TrieNode*> children;
+	TrieNode* failure;
 	bool isWord;
 	int index;//这个query string是第几号
 };
@@ -40,7 +42,7 @@ int main(){
 					break;
 				}
 			if(!inTrie){
-				TrieNode* pNewNode=new TrieNode{queryStr[j], {}, false};
+				TrieNode* pNewNode=new TrieNode{queryStr[j], p ,{}, nullptr, false};
 				p->children.push_back(pNewNode);
 				p=pNewNode;
 			}
@@ -56,12 +58,50 @@ int main(){
 	* 假设当前节点为father，其孩子节点记为child。求child的Fail指针时，首先我们要找到其father的Fail指针所指向的节点,假如是t的话，
  	* 我们就要看t的孩子中有没有和child节点所表示的字母相同的节点，如果有的话，这个节点就是child的fail指针，如果发现没有，
  	* 则需要找father->fail->fail这个节点，然后重复上面过程，如果一直找都找不到，则child的Fail指针就要指向root。*/
-
+	queue<TrieNode*> bfsQue,bfsQue2;
+	bfsQue.push(trieRoot);
+	while (!bfsQue.empty()){
+		while (!bfsQue.empty()) {
+			auto p=bfsQue.front();
+			bfsQue.pop();
+			for(auto pch:p->children)
+				bfsQue2.push(pch);
+			auto pf=p->parent->failure;
+			bool foundFailure= false;
+			while(pf!= nullptr){
+				for(auto pch:pf->children)
+					if(pch->ch==p->ch){
+						p->failure=pch;
+						foundFailure= true;
+						break;
+					}
+				pf=pf->failure;
+			}
+			if(!foundFailure)
+				p->failure=trieRoot;
+		}
+		queue<TrieNode*> emptyQue;
+		bfsQue.swap(emptyQue);
+		bfsQue.swap(bfsQue2);
+	}
+	//use dfs iterate over the queen tree to get answer
 	stack<QueenNode*> iterStack;
 	iterStack.push(&queens[1]);
+	auto state=trieRoot;
 	while(!iterStack.empty()){
-		auto pTrie=trieRoot;
 		auto pQueen=iterStack.top();
-
+		bool foundChar= false;
+		for(auto child:state->children)
+			if(child->ch==pQueen->N){
+				state=child;
+				foundChar= true;
+				if(state->isWord)
+					++wordCount[state->index];
+				break;
+			}
+		if(!foundChar)
+			state=state->failure;
 	}
+	for(auto i:wordCount)
+		cout<<i<<endl;
 }
