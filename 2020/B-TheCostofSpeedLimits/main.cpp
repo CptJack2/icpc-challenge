0cd8bd1d8c8b751852ad79a95dfe50397d5c8dfc
CptJack <844501812@qq.com>
2022-03-28 21:55:09 +0800

    B

--- 2020/B-TheCostofSpeedLimits/main.cpp
+++ 2020/B-TheCostofSpeedLimits/main.cpp
@@ -1,32 +1,31 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include "bits/stdc++.h"
 using namespace std;
 
-int64_t N, C;
-vector<vector<pair<int64_t, int64_t>>> g;
-vector<int64_t> alls;
+int N, C;
+vector<vector<pair<int, int>>> graph;
+vector<int> allUniqueSpeed;
 
-pair<int64_t, vector<int64_t>> doit(int x, int prev) {
-	int64_t mxs = 0, signcost = 0, ps = 0;
-	for (auto [y, s] : g[x]) {
+//return the best cost for subtree, and even cost under corresponding speed in allSpeed
+pair<int, vector<int>> dfs(int x, int prev) {
+	int mxs = 0, signcost = 0, ps = 0;
+	for (auto [y, s] : graph[x]) {
 		mxs = max(mxs, s);
 		if (y == prev) ps = s;
 	};
-	if (g[x].size() > 1) signcost += C * g[x].size();
-	vector<int64_t> v;
-	for (auto [y, s] : g[x]) if (y != prev) {
-			auto [c2, v2] = doit(y, x);
-			if (!v.size()) v.resize(alls.size());
-			for (int i = 0; i < alls.size(); i++)
-				v[i] += v2[i] + alls[i]-s;
+	if (graph[x].size() > 1) signcost += C * graph[x].size();
+	vector<int> v;
+	for (auto [y, s] : graph[x]) if (y != prev) {
+			auto [c2, v2] = dfs(y, x);
+			if (!v.size()) v.resize(allUniqueSpeed.size());
+			for (int i = 0; i < allUniqueSpeed.size(); i++)
+				v[i] += v2[i] + allUniqueSpeed[i] - s;
 			signcost += c2;
 		};
-	if (!v.size()) v.resize(alls.size());
-	int64_t evencost = signcost;
-	for (int i = 0; i < alls.size(); i++) {
-		if (alls[i] >= mxs) {
-			evencost = min(evencost, v[i] + (prev ? alls[i]-ps : 0));
+	if (!v.size()) v.resize(allUniqueSpeed.size());
+	int evencost = signcost;
+	for (int i = 0; i < allUniqueSpeed.size(); i++) {
+		if (allUniqueSpeed[i] >= mxs) {
+			evencost = min(evencost, v[i] + (prev ? allUniqueSpeed[i] - ps : 0));
 			v[i] = min(v[i], signcost);
 		} else {
 			v[i] = signcost;
@@ -36,19 +35,17 @@ pair<int64_t, vector<int64_t>> doit(int x, int prev) {
 }
 
 int main() {
-	int64_t U, V, S;
-	while (cin >> N >> C) {
-		g.clear();
-		g.resize(N+1);
-		for (int i = 1; i < N; i++) {
-			cin >> U >> V >> S;
-			g[U].push_back({V, S});
-			g[V].push_back({U, S});
-			alls.push_back(S);
-		}
-		sort(alls.begin(), alls.end());
-		alls.erase(unique(alls.begin(), alls.end()), alls.end());
-		auto [c, v] = doit(1, 0);
-		cout << c << endl;
+	int u, v, s;
+	cin >> N >> C;
+	graph.resize(N + 1);
+	for (int i = 1; i < N; i++) {
+		cin >> u >> v >> s;
+		graph[u].push_back({v, s});
+		graph[v].push_back({u, s});
+		allUniqueSpeed.push_back(s);
 	}
+	sort(allUniqueSpeed.begin(), allUniqueSpeed.end());
+	allUniqueSpeed.erase(unique(allUniqueSpeed.begin(), allUniqueSpeed.end()), allUniqueSpeed.end());
+	auto[c, v] = dfs(1, 0);
+	cout << c << endl;
 }
