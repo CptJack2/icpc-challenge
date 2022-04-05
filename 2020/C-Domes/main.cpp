8c0578e1bdbb3fa99238ae64bf20558b9ff8b695
CptJack <844501812@qq.com>
2022-04-05 10:36:25 +0800

    C

--- 2020/C-Domes/main.cpp
+++ 2020/C-Domes/main.cpp
@@ -34,20 +34,18 @@ int main(){
 	for(int i=0;i<n;++i)
 		cin>>permutaion[i];
 	//
-	vector<Point> polygon={{0,0},{0,dy},{dx,dy},{dx,0}};
+	list<Point> polygon={{0,0},{0,dy},{dx,dy},{dx,0}};
 	for(int i=0;i<=n-2;++i){
 		auto &p1=domeLocs[permutaion[i]], &p2=domeLocs[permutaion[i+1]];
-		//求通过per[i],per[i+1]的直线
-		Line l= GetLineFromTwoPoints(p1,p2);
-		//求polygon和直线的交点, 或者判断polygon是否在直线的一侧
-		Point normalVector=Point{p1.y-p2.y,p2.x-p1.x};//p1p2的法向量
-		Point p3=p1+normalVector;
-		//法向量要指向p1p2的右侧
-		if(CrossProduction(p3,p2-p1)<0)
-			normalVector.x=-normalVector.x,
-			normalVector.y=-normalVector.y;
 		//取直线一边的polygon为新多边形
-		for(auto& pPoly:polygon)
+		int j=0;
+		do{
+			double cpj=CrossProduction(p2-p1,polygon[j]-p1),
+				cpjp1=CrossProduction(p2-p1,polygon[j+1]-p1);
+			if(cpj>0)
+
+		}
+
 	}
 
 }
