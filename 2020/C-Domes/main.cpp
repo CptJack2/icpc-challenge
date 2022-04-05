30c7c86177d7c6ca0c2d1e0edbb738419931e6c5
CptJack <844501812@qq.com>
2022-04-05 15:46:59 +0800

    C

--- 2020/C-Domes/main.cpp
+++ 2020/C-Domes/main.cpp
@@ -1,10 +1,7 @@
+/*计算几何题,如果要dome di出现在dj左边,那么只有向量didj右侧的区域是可行的.
+ * 初始设区域为整个广场,对每一对didj直线分别更新区域(一定是一个凸多边形),最后算面积*/
 #include "bits/stdc++.h"
 using namespace std;
-
-struct Line{//Ax+By+C=0
-	double A,B,C;
-	bool bigger;//region Ax+By+C>0 or Ax+By+C<0
-};
 
 struct Point{
 	double x,y;
@@ -22,6 +19,7 @@ vector<int> permutaion;
 double CrossProduction(const Point& a,const Point& b){return a.x*b.y-a.y*b.x;}
 
 int main(){
+	//read in data
 	cin>>dx>>dy>>n;
 	domeLocs.resize(n);
 	permutaion.resize(n);
@@ -29,21 +27,23 @@ int main(){
 		cin>>domeLocs[i].x>>domeLocs[i].y;
 	for(int i=0;i<n;++i)
 		cin>>permutaion[i];
-	
+	//update available zone
 	vector<Point> polygon={{0,0},{0,dy},{dx,dy},{dx,0}}, polygon2;
 	for(int i=0;i<=n-2;++i)
-	for(int ii=i+1;ii<=n-1;++ii){
-		auto &p1=domeLocs[permutaion[i]-1], &p2=domeLocs[permutaion[ii]-1];
+	for(int j= i + 1; j <= n - 1; ++j){//这里排列中每一对i<j的didj都要处理一遍,否则可能出现i在j左边,j在k左边,但i不在k左边的情况
+		auto &p1=domeLocs[permutaion[i]-1], &p2=domeLocs[permutaion[j] - 1];
 		polygon2.clear();
 		//取直线一边的polygon为新多边形
-		for(int j=0;j<polygon.size();++j){
-			auto& p3=polygon[j], p4=j!=polygon.size()-1?polygon[j+1]:polygon[0];
+		for(int k=0; k < polygon.size(); ++k){
+			auto& p3=polygon[k], p4= k != polygon.size() - 1 ? polygon[k + 1] : polygon[0];
+			//通过叉积判断点和直线p1p2的关系
 			auto cp3=CrossProduction(p2-p1,p3-p1), cp4=CrossProduction(p2-p1,p4-p1);
-			//p3点在向量p1p2的右侧
+			//p3点在向量p1p2的右侧,在新可行域中保留
 			if(cp3<=0)
 				polygon2.push_back(p3);
-			//p3和p4中间有新的交点
+			//过p1p2的直线和过p3p4的线段中间有交点,将其加入新可行域
 			if(cp3>0 && cp4<0 || cp3<0 && cp4>0){
+				//计算交点
 				double x1 = p1.x, y1 = p1.y;
 				double x2 = p2.x, y2 = p2.y;
 				double x3 = p3.x, y3 = p3.y;
@@ -57,7 +57,7 @@ int main(){
 		if(polygon.empty())
 			break;
 	}
-	//算面积
+	//通过多边形所有相邻两点的叉积算面积
 	double area=0;
 	for(auto it=polygon.begin();it!=polygon.end();++it){
 		auto nit=next(it);
