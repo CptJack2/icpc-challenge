a7d860a6ec6cb0d6fcf808112a1792efe80f457d
CptJack <844501812@qq.com>
2022-04-10 10:51:22 +0800

    bugfix

--- 2020/F-LeyLines/main.cpp
+++ 2020/F-LeyLines/main.cpp
@@ -1,18 +1,19 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-const double pi=M_PI;
+const long double pi=M_PI;
 
 struct Vec{
-	double x,y;
+	long double x,y;
 	Vec operator-(const Vec& b){return Vec{x-b.x,y-b.y};}
-	double len(){return hypot(x,y);}
-	double theta(){return fmod(atan2(y,x)+2*pi,2*pi);}//atan2 return (-pi,pi], make it (0,2pi]
+	long double len(){return hypot(x,y);}
+	long double theta(){return fmod(atan2(y,x)+2*pi,2*pi);}//atan2 return (-pi,pi], make it (0,2pi]
 };
 
 struct event{
-	double angle;
+	long double angle;
 	bool enter;//true enter, false leave
+	int index;//for debug
 };
 
 int main(){
@@ -35,23 +36,24 @@ int main(){
 			if(v.len()<=t)
 				++Ps;
 			else{
-				auto adjustAngle=[&](double ang)->double{//make angle in [0,2pi] to [0,pi]
-					if(ang>pi)
+				auto adjustAngle=[&](long double ang)->long double{//θ∈(0,2pi], φ∈[0,pi/2), ang=θ±φ∈(-pi/2,5/2pi], adjust them to (-pi/2,pi/2]
+					ang=fmod(ang+pi/2+2*pi,2*pi)-pi/2;//(-pi/2,3/2pi]
+					if(ang>pi/2)
 						return ang-pi;
 					return ang;
 				};
-				double theta=v.theta(),
-					phi= asin(double(t)/v.len());//(0,pi/2)
-				if(theta<=pi)
-					onTop.push_back(event{adjustAngle(theta),true}),
-					onTop.push_back(event{adjustAngle(theta+phi),false}),
-					onBottom.push_back(event{adjustAngle(theta-phi),true}),
-					onBottom.push_back(event{adjustAngle(theta),false});
+				long double theta=v.theta(),
+					phi= asin((long double)t/v.len());//[0,pi/2)
+				if(theta<=pi/2 || theta>3*pi/2)
+					onTop.push_back(event{adjustAngle(theta),true,j}),
+					onTop.push_back(event{adjustAngle(theta+phi),false,j}),
+					onBottom.push_back(event{adjustAngle(theta-phi),true,j}),
+					onBottom.push_back(event{adjustAngle(theta),false,j});
 				else
-					onTop.push_back(event{adjustAngle(theta-phi),true}),
-					onTop.push_back(event{adjustAngle(theta),false}),
-					onBottom.push_back(event{adjustAngle(theta),true}),
-					onBottom.push_back(event{adjustAngle(theta+phi),false});
+					onTop.push_back(event{adjustAngle(theta-phi),true,j}),
+					onTop.push_back(event{adjustAngle(theta),false,j}),
+					onBottom.push_back(event{adjustAngle(theta),true,j}),
+					onBottom.push_back(event{adjustAngle(theta+phi),false,j});
 			}
 		}
 		auto cmp=[&](const event& a,const event& b){//first key angel, second key the point that is entering comes first
@@ -69,8 +71,10 @@ int main(){
 					--Ps;
 			return maxP;
 		};
-		ans=max(ans, findMax(onTop));
-		ans=max(ans, findMax(onBottom));
+		int fmt=findMax(onTop),
+			fmb=findMax(onBottom);
+		ans=max(ans, fmt);
+		ans=max(ans, fmb);
 	}
 	cout<<ans;
 	return 0;
