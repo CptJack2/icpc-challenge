ebddb420f304c1cc3e42d2e85a4b26de929ccc77
CptJack <844501812@qq.com>
2022-04-13 15:47:38 +0800

    面试 tmp

--- 2020/H-QCQC/main.cpp
+++ 2020/H-QCQC/main.cpp
@@ -2,50 +2,48 @@
 using namespace std;
 
 //深度优先搜索去匹配
-pair<bool,string> dfs(const string& B,const int Bindex, int availableDigits[10], bool useMin){
+bool dfs(string& A, const string& B, const int index, int availableDigits[10], bool useMin){
 	string retStr="";
 	//前面的串小于B了,没有最大限制,只需要一路把剩下的数字按大到小排下去
 	if(!useMin){
-		for(int i=Bindex;i<B.size();++i)
+		for(int i=index; i < B.size(); ++i)
 			for(int j=9;j>=0;--j)
 				if(availableDigits[j]!=0){
 					--availableDigits[j],
-					retStr+=char(j+'0');
+					A[index]=char(j+'0');
 					break;
 				}
-		return {true,retStr};
+		return true;
 	}
 	for(int i=9;i>=0;--i)
 		//判断当前可以填的最大数字
 		if(availableDigits[i]!=0 &&
-			i <= B[Bindex] - '0'){//如果这一位放到最大了,后面的数字都有限制,
-			if(Bindex==0 && i==0)continue;//首位不能放0
+			i <= B[index] - '0'){//如果这一位放到最大了,后面的数字都有限制,
+			if(index == 0 && i == 0)continue;//首位不能放0
 			//更新可用数字的数量
 			--availableDigits[i];
 			//递归去解
-			auto ret=dfs(B,Bindex+1,availableDigits,i==B[Bindex]-'0');
-			if(ret.first){//有解的话就直接返回
-				string rtmp;
-				rtmp+=char(i+'0');
-				return {true,rtmp+ret.second};
-			}else{
+			auto ret=dfs(A,B, index + 1, availableDigits, i == B[index] - '0');
+			if(ret){//有解的话就直接返回
+				A[index]=char(i+'0');
+				return true;
+			}else
 				//回滚可用数字的数量
 				++availableDigits[i];
-			}
 		}
-	return {false,""};
+	return false;
 }
 
 string solve(const string& A, const string& B){//输出A排列后最大可能值,无解输出"impossible",要满足A<B
 	int availableDigits[10];
 	memset(availableDigits,0,sizeof(int)*10);
+	string retStr;
 	for(auto c:A)
 		availableDigits[c-'0']++;
 	//A比B长,不可能有解
 	if(A.size()>B.size())return "impossible";
 	//A比B短,直接数字大到小排下去
 	else if(A.size()<B.size()){
-		string retStr;
 		for(int i=0;i<A.size();++i)
 			for(int j=9;j>=0;--j)
 				if(availableDigits[j]!=0){
@@ -57,9 +55,10 @@ string solve(const string& A, const string& B){//输出A排列后最大可能值
 	}else if(A.empty())//corner case
 		return "";
 	//len(A)==len(B),需要递归去解
-	auto ret=dfs(B,0,availableDigits,true);
-	if(ret.first)
-		return ret.second;
+	retStr=A;
+	auto ret=dfs(retStr,B,0,availableDigits,true);
+	if(ret)
+		return retStr;
 	else
 		return "impossible";
 }
