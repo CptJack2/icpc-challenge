ee5f3b286b0eedf532306447419f215581dfd6cc
CptJack <844501812@qq.com>
2022-04-16 10:55:57 +0800

    H tmp

--- 2020/H-QCQC/main.cpp
+++ 2020/H-QCQC/main.cpp
@@ -1,89 +1,120 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-//深度优先搜索去匹配
-bool dfs(string& A, const string& B, const int index, int availableDigits[10], bool useMin){
-	if(index==B.size())return true;
-	string retStr="";
-	//前面的串小于B了,没有最大限制,只需要一路把剩下的数字按大到小排下去
-	if(!useMin){
-		for(int i=index; i < B.size(); ++i)
-			for(int j=9;j>=0;--j)
-				if(availableDigits[j]!=0){
-					--availableDigits[j],
-					A[i]=char(j+'0');
-					break;
-				}
-		return true;
-	}
-	for(int i=9;i>=0;--i)
-		//判断当前可以填的最大数字
-		if(availableDigits[i]!=0 &&
-			i <= B[index] - '0'){//如果这一位放到最大了,后面的数字都有限制,
-			if(index == 0 && i == 0)continue;//首位不能放0
-			//更新可用数字的数量
-			--availableDigits[i];
-			//递归去解
-			auto ret=dfs(A,B, index + 1, availableDigits, i == B[index] - '0');
-			if(ret){//有解的话就直接返回
-				A[index]=char(i+'0');
-				return true;
-			}else
-				//回滚可用数字的数量
-				++availableDigits[i];
-		}
-	return false;
-}
-
-string solve(const string& A, const string& B){//输出A排列后最大可能值,无解输出"impossible",要满足A<B
-	int availableDigits[10];
-	memset(availableDigits,0,sizeof(int)*10);
-	string retStr;
-	for(auto c:A)
-		availableDigits[c-'0']++;
-	//A比B长,不可能有解
-	if(A.size()>B.size())return "impossible";
-	//A比B短,直接数字大到小排下去
-	else if(A.size()<B.size()){
-		for(int i=0;i<A.size();++i)
-			for(int j=9;j>=0;--j)
-				if(availableDigits[j]!=0){
-					--availableDigits[j],
-					retStr+=char(j+'0');
-					break;
-				}
-		return retStr;
-	}else if(A.empty())//corner case
-		return "";
-	//len(A)==len(B),需要递归去解
-	retStr=A;
-	auto ret=dfs(retStr,B,0,availableDigits,true);
-	if(ret)
-		return retStr;
-	else
-		return "impossible";
-}
+typedef list<int> Component;//machines with same state
 
 int main(){
-	struct testCase{
-		string A,B,ans;
-	};
-	vector<testCase> testCases={
-		{"1234","3000","2431"},
-		{"4321","1234","1234"},//AB一样的
-		{"4321","1235","1234"},//直接摆
-		{"387656","387654","387566"},	//中间回溯
-		{"766","765","676"},	//开头回溯
-		{"","",""},	//AB都空
-		{"789","8888","987"},	//A比B短
-		{"78900","8888","impossible"},//A比B长
-		{"5999","5555","impossible"},//AB等长, 但无解
-	};
-	int a=1;
-	for(auto& tc:testCases){
-		auto ret=solve(tc.A,tc.B);
-		if(ret!=tc.ans)
-			cout<<"failed"<<endl;
+	int batch;
+	cin>>batch;
+	for (int i = 0; i < batch; ++i) {
+		int n;
+		cin>>n;
+		list<Component> active,leftOver;
+		list<pair<Component,Component>> paired;
+		for (int j = 1; j <= n; ++j) {
+			Component tc;
+			tc.push_back(j);
+			active.push_back(tc);
+		}
+		auto query = [&](const vector<int> &queries) -> vector<bool> {
+			cout << "test";
+			for (auto &q:queries)
+				cout << " " << q;
+			vector<bool> ret(n+1, false);
+			char c;
+			for (int i = 1; i <= n; ++i)
+				cin >> c,
+				ret[queries[i]] = c == '1';
+			return ret;
+		};
+		//phase 1, query active
+		while(active.size()>1) {
+			auto it = active.begin(), nit = it;
+			vector<int> queries(n+1,0);
+			while (1) {
+				nit = next(it);
+				if (nit == active.end()) {
+					leftOver.splice(leftOver.end(), active, it);
+					break;
+				}
+				queries[it->front()] = nit->front();
+				it = next(nit);
+				if (it == active.end())break;
+			}
+			if(it!=active.end())
+				if(!leftOver.empty()) {//at most 2 elements in leftOver and component in active always has more elements
+					queries[it->front()] = leftOver.front().front();
+					queries[leftOver.front().front()] =it->front();
+//					if (leftOver.size() == 2) //deal with the second left over component
+//						queries[*next(it->begin())] = next(leftOver.begin())->front(),
+//						queries[next(leftOver.begin())->front()] = *next(it->begin());
+				}else{
+					leftOver.splice(leftOver.end(),active,it);
+					active.erase(it);
+					it=active.end();
+				}
+			auto res = query(queries);
+			if(it!=active.end() && !leftOver.empty()){
+				if(res[it->front()] == res[leftOver.front().front()] && res[it->front()]==true)
+					leftOver.front().splice(leftOver.front().end(),*it);
+				else if(leftOver.size()==2)
+					next(leftOver.begin())->splice(leftOver.front().end(),*it);
+				else
+					leftOver.splice(leftOver.end(),active,it);
+				active.erase(it),
+				it=active.end();
+			}
+			it = active.begin();
+			while (it!=active.end()) {
+				nit = next(it);
+				if(res[it->front()]==res[nit->front()] && res[it->front()]==true){
+					it->splice(it->begin(),*nit);
+					active.erase(nit);
+					it=next(it);
+				}else{
+					paired.emplace_back(*it,*nit);
+					auto tit=next(nit);
+					active.erase(it);
+					active.erase(nit);
+					it=tit;
+				}
+			}
+		}
+		//intermediate phase
+		Component good,bad;
+		vector<int> queries(n,0);
+		if(active.size()==1){
+			good.splice(good.begin(),active.front());
+			active.clear();
+			if(!leftOver.empty()){
+				queries[good.front()]=leftOver.front().front();
+				if(leftOver.size()==2)
+					queries[*next(good.begin())]=next(leftOver.begin())->front();
+				auto res=query(queries);
+				if(res[leftOver.front().front()])
+					good.splice(good.end(),leftOver.front());
+				else
+					bad.splice(bad.end(),leftOver.front());
+				if(leftOver.size()==2)
+					if(res[next(leftOver.begin())->front()])
+						good.splice(good.end(),*next(leftOver.begin()));
+					else
+						bad.splice(bad.end(),*next(leftOver.begin()));
+				leftOver.clear();
+			}
+		}else{
+			auto biggestLeftOver=leftOver.begin();
+			for(auto it=leftOver.begin();it!=leftOver.end();++it)
+				if(it->size() > biggestLeftOver->size())
+					biggestLeftOver=it;
+			good.splice(good.end(),*biggestLeftOver);
+			leftOver.erase(biggestLeftOver);
+			for(auto& l:leftOver)
+				bad.splice(bad.end(),l);
+			leftOver.clear();
+		}
+		//phase 2
+
 	}
-	cout<<"aaa";
+	return 0;
 }
