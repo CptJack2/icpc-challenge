d111a7a6a40e9b080ac998d59f3f8d2adda94bc6
CptJack <844501812@qq.com>
2022-04-17 11:46:59 +0800

    H comment

--- 2020/H-QCQC/main.cpp
+++ 2020/H-QCQC/main.cpp
@@ -26,12 +26,15 @@ int main(){
 			for (int i = 1; i <= n; ++i)
 				cin >> c,
 				ret[queries[i]] = c == '1';
+			bool db=true;
+			while(db){}
 			return ret;
 		};
 		//phase 1, query active
 		while(active.size()>1) {
 			auto it = active.begin(), nit = it;
 			vector<int> queries(n+1,0);
+			//group active components into pairs for query
 			while (1) {
 				nit = next(it);
 				if (nit == active.end()) {
@@ -43,19 +46,18 @@ int main(){
 				it = next(nit);
 				if (it == active.end())break;
 			}
+			//if active has odd components, put it to left over
 			if(it!=active.end())
 				if(!leftOver.empty()) {//at most 2 elements in leftOver and component in active always has more elements
 					queries[it->front()] = leftOver.front().front();
 					queries[leftOver.front().front()] =it->front();
-//					if (leftOver.size() == 2) //deal with the second left over component
-//						queries[*next(it->begin())] = next(leftOver.begin())->front(),
-//						queries[next(leftOver.begin())->front()] = *next(it->begin());
 				}else{
 					leftOver.splice(leftOver.end(),active,it);
 					active.erase(it);
 					it=active.end();
 				}
-			auto res = query(queries);
+			auto res = query(queries);//do query
+			//merge the remaining active with left over
 			if(it!=active.end() && !leftOver.empty()){
 				if(res[it->front()] == res[leftOver.front().front()] && res[it->front()]==true)
 					leftOver.front().splice(leftOver.front().end(),*it);
@@ -66,6 +68,7 @@ int main(){
 				active.erase(it),
 				it=active.end();
 			}
+			//put active pair to paired or group them together according to query result
 			it = active.begin();
 			while (it!=active.end()) {
 				nit = next(it);
