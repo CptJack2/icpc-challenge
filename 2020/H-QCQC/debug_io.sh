bb94ca5b34bda7c3aac21c49631cfe0d313eb48c
CptJack <844501812@qq.com>
2022-04-17 11:19:20 +0800

    H debug_io.sh

--- 2020/H-QCQC/debug_io.sh
+++ 2020/H-QCQC/debug_io.sh
@@ -1,20 +1,35 @@
 #! /bin/bash
 
-rm input.txt
-touch input.txt
-rm output.txt
-touch output.txt
+function reader() {
+  rm input.txt
+  touch input.txt
+  lnum=$(wc -l input.txt|awk '{print $1}')
 
-lnum=$(wc -l input.txt|awk '{print $1}')
-
-while [ true ]; do
   while [ true ]; do
-    newlnum=$(wc -l input.txt|awk '{print $1}')
-    if [[ $newlnum != $lnum ]];then
-      lnum=$newlnum;
-      break;
+    while [ true ]; do
+      newlnum=$(wc -l input.txt|awk '{print $1}')
+      if [[ $newlnum != $lnum ]];then
+        lnum=$newlnum;
+        break;
+      fi
+    done
+    newl=$(tail -n1 input.txt)
+    echo $newl
+  done
+}
+
+function writer() {
+  rm output.txt
+  touch output.txt
+
+  while [ true ]; do
+    read out
+    if [[ $out != "" ]];then
+      echo $out>>output.txt
     fi
   done
-  newl=$(tail -n1 input.txt)
-  echo $newl >> output.txt
-done
+}
+
+reader &
+writer
+wait
