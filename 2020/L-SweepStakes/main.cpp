80315952af8d3422304fe2e20e1449de4fb6170e
CptJack <844501812@qq.com>
2022-04-26 10:01:29 +0800

    L

--- 2020/L-SweepStakes/main.cpp
+++ 2020/L-SweepStakes/main.cpp
@@ -30,12 +30,20 @@ struct ProbabilityCalc{
 	
 	ProbabilityCalc():base(0),prob({1}),ignoredUB(1){}
 	
-	//查询数遇到分叉的时候更新格子的集合,重新计算概率
+	//查询树遇到分叉的时候更新格子的集合,重新计算概率
 	ProbabilityCalc(const ProbabilityCalc& copy){
 		base=copy.base+copy.ignoredLB;
 		prob.resize(copy.ignoredUB-copy.ignoredLB);
 		std::copy(copy.prob.begin()+copy.ignoredLB,copy.prob.begin()+copy.ignoredUB, prob.begin());
 		ignoredUB=prob.size();
+	}
+
+	double operator[](int i) const{
+		if(i<base+ignoredLB)
+			return 0;
+		if(i>=base+ignoredUB)
+			return 0;
+		return prob[i-base];
 	}
 };
 
@@ -53,7 +61,7 @@ int main(){
 		cin>>probRow[i];
 	for (int i = 0; i <n ; ++i)
 		cin>>probCol[i];
-	vector<QueryNode> queries(q+1);
+	vector<QueryNode> queries(q+1);//最后一个是所有m*n个格子的全集,来保证查询树根上一定是格子全集
 	for (int i = 0; i < q; ++i) {
 		int s;
 		cin>>s;
@@ -81,12 +89,40 @@ int main(){
 	auto root=buildTree(0,q+1);
 	//计算概率
 	double pTMinesInField=0;
-	function<void(int,int)> calcProbability=[&](int s,int e){
-		if(s+1==e){
-
-		}else{
-
+	function<void(QueryNode*,const ProbabilityCalc&)>
+	calcProbability=[&](QueryNode* pnode,const ProbabilityCalc& pNotInSet){
+		if(pnode->left== nullptr && pnode->right==nullptr){//到达查询树的叶子
+			if(pnode->cords.size()==m*n)
+				return;//辅助用的格子全集
+			ProbabilityCalc pInSet;
+			pInSet.calcProb(pnode->cords);
+			//整个雷场中恰好有t枚地雷的概率,第一次到树叶的时候计算
+			if(pTMinesInField==0)
+				for (int i = 0; i <= t; ++i)
+					pTMinesInField+=pInSet[i]*pNotInSet[t-i];
+			for (int i = 0; i <= pnode->cords.size(); ++i) {
+				//贝叶斯公式,已知雷场共有t个雷的情况下,要查询的格子里面有i个雷的概率,等于查询格子里面有i个的概率*全集减去查询集中有t-i个的概率/整个雷场有t个的概率
+				double pIMinesInQueryCords= pInSet[i] * pNotInSet[t-i] / pTMinesInField;
+				cout<<setprecision(12)<<pIMinesInQueryCords<<" ";
+			}
+			cout<<endl;
+		}else{//树中节点,向左右分支计算not in set的概率
+			ProbabilityCalc pLNotInSet(pNotInSet),pRNotInSet(pNotInSet);
+			set<pair<int,int>> lNotInSet,rNotInSet;
+			//左递归
+			for(auto pi:pnode->cords)
+				if(!pnode->left->cords.count(pi))
+					lNotInSet.insert(pi);
+			pLNotInSet.calcProb(lNotInSet);
+			calcProbability(pnode->left,pLNotInSet);
+			//右递归
+			for(auto pi:pnode->cords)
+				if(!pnode->right->cords.count(pi))
+					rNotInSet.insert(pi);
+			pRNotInSet.calcProb(rNotInSet);
+			calcProbability(pnode->right,pRNotInSet);
 		}
+		calcProbability(root,ProbabilityCalc());
 	};
 	return 0;
 }
