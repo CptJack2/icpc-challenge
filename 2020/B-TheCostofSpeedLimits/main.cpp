abf32b4724f52d7ae3e3bd86d13fdf514c6b72f1
CptJack <844501812@qq.com>
2022-06-24 19:57:29 +0800

    2020 B

--- 2020/B-TheCostofSpeedLimits/main.cpp
+++ 2020/B-TheCostofSpeedLimits/main.cpp
@@ -2,26 +2,46 @@
 
 using namespace std;
 
-vector<map<int,int>> speeds;
+vector<map<int,int>> graph;
+vector<int> allSpeed;
+int n,c;
 
 pair<int,vector<int>> dfs(int node, int parent){
-	for(auto [ch,s]:speeds[node])
+	int signCost=c*graph[node].size();
+	vector<int> costs(allSpeed.size(),0);
+	int maxSpeed=0, parS=graph[node][parent];
+	for(auto [ch,s]:graph[node])maxSpeed=max(maxSpeed,s);
+	for(auto [ch,s]:graph[node])
 		if(ch!=parent){
-			auto chR=dfs(ch,node);
-
+			auto [chBC,chC]=dfs(ch,node);
+			signCost+=chBC;
+			for(int i=0;i<allSpeed.size();i++)
+				costs[i]+=chC[i];
 		}
+	int bestCost=signCost;
+	for(int i=0;i<allSpeed.size();i++)
+		if(allSpeed[i]<maxSpeed)
+			costs[i]=signCost;
+		else
+			costs[i]+=allSpeed[i]-parS,
+			bestCost=min(bestCost,costs[i]);
+	return {bestCost,costs};
 }
 
 int main(){
-	int n,c;
 	cin>>n>>c;
-	speeds.resize(n+1);
+	graph.resize(n + 1);
+	set<int> uniqueSpeeds;
 	for(int i=0;i<n-1;i++){
 		int u,v,s;
 		cin>>u>>v>>s;
-		speeds[u][v]=s;
-		speeds[v][u]=s;
+		graph[u][v]=s;
+		graph[v][u]=s;
+		uniqueSpeeds.insert(s);
 	}
-	dfs(1,0);
+	for(auto s:uniqueSpeeds)allSpeed.push_back(s);
+	graph[1][0]=0;
+	auto [bc,_]=dfs(1,0);
+	cout<<bc;
 	return 0;
 }
