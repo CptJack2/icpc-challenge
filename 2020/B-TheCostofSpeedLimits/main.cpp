3a242ee4ae8334b332e1f4c02c79ee1cbd8a82f8
CptJack <844501812@qq.com>
2022-06-24 20:13:03 +0800

    2020 B

--- 2020/B-TheCostofSpeedLimits/main.cpp
+++ 2020/B-TheCostofSpeedLimits/main.cpp
@@ -7,7 +7,7 @@ vector<int> allSpeed;
 int n,c;
 
 pair<int,vector<int>> dfs(int node, int parent){
-	int signCost=c*graph[node].size();
+	int signCost=graph[node].size()>1?c*graph[node].size():0;
 	vector<int> costs(allSpeed.size(),0);
 	int maxSpeed=0, parS=graph[node][parent];
 	for(auto [ch,s]:graph[node])maxSpeed=max(maxSpeed,s);
@@ -16,15 +16,16 @@ pair<int,vector<int>> dfs(int node, int parent){
 			auto [chBC,chC]=dfs(ch,node);
 			signCost+=chBC;
 			for(int i=0;i<allSpeed.size();i++)
-				costs[i]+=chC[i];
+				costs[i]+=chC[i]+allSpeed[i]-s;
 		}
 	int bestCost=signCost;
 	for(int i=0;i<allSpeed.size();i++)
 		if(allSpeed[i]<maxSpeed)
-			costs[i]=signCost;
+			costs[i]=signCost+allSpeed[i]-parS;
 		else
 			costs[i]+=allSpeed[i]-parS,
-			bestCost=min(bestCost,costs[i]);
+			bestCost=min(bestCost,costs[i]),
+			costs[i]=min(costs[i],signCost+allSpeed[i]-parS);
 	return {bestCost,costs};
 }
 
