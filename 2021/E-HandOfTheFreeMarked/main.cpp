3cf2f498a5fd0696cb8c53025fab6ee3b1786efb
CptJack <844501812@qq.com>
2023-09-21 13:47:06 +0800

    refactor

--- 2021/E-HandOfTheFreeMarked/main.cpp
+++ 2021/E-HandOfTheFreeMarked/main.cpp
@@ -6,13 +6,42 @@
 using namespace std;
 int K, M;
 
-vector<int> col;//M个col和各自的牌数量
+vector<int> markedNum;//M个col和各自的牌数量
 double hands = 0.0, matched = 0.0;
-vector<int> v;//控制分为K个牌分别属于M个类别中的哪一类
+long long totalCombinations = 0, totalArrangements = 0;
+vector<int> cardType;//控制分为K个牌分别属于M个类别中的哪一类
 
+void calaculateProbability(){
+    double h = 1.0, m = 0.0;
+    vector<double> fact(K);
+    //求从Mi中取K个的组合数
+    for (int ii = 0, last = -1, n = 1; ii < K; ii++) {
+        if (cardType[ii] == last) n++; else n = 1;
+        last = cardType[ii];
+        fact[ii] = double(markedNum[cardType[ii]] - (n - 1)) / n;
+        h *= fact[ii];
+    }
+    if (h == 0.0) return;
+    for (int ii = 0; ii < K; ii++)
+        if (ii == K - 1 || cardType[ii] != cardType[ii + 1]) {
+            m += h / fact[ii];//C 52 4
+        }
+    for (int ii = 1; ii <= K - 1; ii++) m *= ii;//A 52 4
+    hands += h;
+    matched += min(h, m);
+}
+
+void generateCardType(){
+    for (int i = 0; i < K; ++i) {
+        for (cardType[i] = 0; cardType[i] < M; cardType[i]++) {
+            if(i>=1 && cardType[i]<cardType[i-1])continue;
+            calaculateProbability();
+        }
+    }
+}
 void rec(int i, int x) {
     if (i < K) {
-        for (v[i] = x; v[i] < M; v[i]++) rec(i + 1, v[i]);
+        for (cardType[i] = x; cardType[i] < M; cardType[i]++) rec(i + 1, cardType[i]);
         return;
     }
     //算一个h和m，是总的hands和matched的一部分
@@ -20,14 +49,14 @@ void rec(int i, int x) {
     vector<double> fact(K);
     //求从Mi中取K个的组合数
     for (int ii = 0, last = -1, n = 1; ii < K; ii++) {
-        if (v[ii] == last) n++; else n = 1;
-        last = v[ii];
-        fact[ii] = double(col[v[ii]] - (n - 1)) / n;
+        if (cardType[ii] == last) n++; else n = 1;
+        last = cardType[ii];
+        fact[ii] = double(markedNum[cardType[ii]] - (n - 1)) / n;
         h *= fact[ii];
     }
     if (h == 0.0) return;
     for (int ii = 0; ii < K; ii++)
-        if (ii == K - 1 || v[ii] != v[ii + 1]) {
+        if (ii == K - 1 || cardType[ii] != cardType[ii + 1]) {
             m += h / fact[ii];//C 52 4
         }
     for (int ii = 1; ii <= K - 1; ii++) m *= ii;//A 52 4
@@ -36,16 +65,19 @@ void rec(int i, int x) {
 }
 
 int main() {
-//    cin >> K >> M;
 //    K=5;M=1;    col=vector<int>{52};
 //    K=3;M=1;    col=vector<int>{10};// A 10 2 / C 10 3
-    K=2;M=2;    col=vector<int>{3,3};//
-    K=3;M=2;    col=vector<int>{5,5};//
+//    K=2;M=2; markedNum=vector<int>{3, 3};//
+//    K=3;M=2; markedNum=vector<int>{5, 5};//
 //    K=3;M=4;col=vector<int>{5,12,1,2};
-    col.resize(M);
-    v.resize(K);
-//    for (int i = 0; i < M; i++) cin >> col[i];
+
+    cin >> K >> M;
+    for (int i = 0; i < M; i++) cin >> markedNum[i];
+
+    markedNum.resize(M);
+    cardType.resize(K, 0);
 
     rec(0, 0);
+//    generateCardType();
     printf("%.12lf\n", matched / hands);
 }
