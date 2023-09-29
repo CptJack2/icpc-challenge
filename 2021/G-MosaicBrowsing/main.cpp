1298b245f9dbc1382d8601ca20424b26e7890f63
CptJack <844501812@qq.com>
2023-09-29 23:23:18 +0800

    2021 G

--- 2021/G-MosaicBrowsing/main.cpp
+++ 2021/G-MosaicBrowsing/main.cpp
@@ -2,21 +2,29 @@
 using namespace std;
 
 typedef bitset<1000> bs;
+//class bs:public array<int,1000>{
+//public:
+//    void set(int ind){
+//
+//    }
+//};
 
-//stringstream cin(R"(3 4
-//1 2 3 4
-//2 3 0 1
-//3 1 2 0
-//)");
-//using ::cin;
+
+//stringstream cin(R"(2 1
+//1
+//2
+//3 4
+//1 2 1 2
+//3 1 2 1
+//1 2 1 2)");
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+//    std::ios_base::sync_with_stdio(false);
+//    std::cin.tie(nullptr);
     int PY, PX, QY, QX, PC, QC;//motif和mosiac最大1000x1000
     ::cin >> PY >> PX;
     //g和cg是根据motif构造的pattern
-    vector<bs> g(PX);//col用数组下标表示,row用bitset位表示
-    vector<vector<bs>> cg(101, vector<bs>(PX));//color
+    vector<bs> g(PX);//col用数组下标表示,row用bitset位表示, 有颜色grid
+    vector<vector<bs>> cg(101, vector<bs>(PX));//color，每个颜色层grid，∑cg=~b
     auto showPat=[&](){
         cout<<"g"<<endl;
         for (int i = 0; i < PX; ++i) {
@@ -64,6 +72,19 @@ int main() {
     //根据mosiac构造的待匹配,都按颜色分层
     vector<vector<bs>> gg(101, vector<bs>(PX));
     vector<vector<int>> cookie(101, vector<int>(PX, -1));
+    auto printBs=[&](const bs& bs1){
+        auto str=bs1.to_string();
+        reverse(str.begin(),str.end());
+        //str=str.substr(0,QX);
+        std::string result;
+        for (size_t i = 0; i < str.length(); i++) {
+            result += str[i];
+            if (i != str.length() - 1) {
+                result += ' ';
+            }
+        }
+        return result;
+    };
     for (int y = 0; y < QY; y++) {
         for (int x = 0; x < QX; x++) {
             ::cin >> QC;//读入mosaic一格颜色
@@ -73,15 +94,31 @@ int main() {
             for (int px = max((PX - 1) - x, 0); px < PX; px++) {
                 int fx = x - (PX - 1) + px;
                 if (fx > QX - PX) break;
-                if (cookie[QC][px] != y) {
+//                printf("px %d QC %d y %d PY %d y<PY %s\n",px,QC,y ,PY,y<PY?"true":"false");
+//                cout<<"g[px] "<<printBs(g[px])<<endl<<"cg[QC][px] "<<printBs(cg[QC][px])<<endl;
+                if (cookie[QC][px] != y) {//这里每次比对motif的px列
                     cookie[QC][px] = y;
-                    if (y < PY) {
+                    if (y < PY) {//g[px] & cg[QC][px] 有色且色不对,&出来那个位置才是1, motif的px列,每一格是否match当前这个QC
                         gg[QC][px] = (g[px] & cg[QC][px]) >> (PY - 1) - y;
                     } else {
                         gg[QC][px] = (g[px] & cg[QC][px]) << y - (PY - 1);
                     }
                 }
+//                for (int c = 1; c <=3 ; ++c) {
+//                    cout << "gg " << c << endl;
+//                    for (int i = 0; i < QY; ++i) {
+//                        cout << printBs(gg[c][i]) <<endl;
+//                    }
+//                    cout << endl;
+//                }
+//                cout<<"gg[QC][px] "<<printBs(gg[QC][px])<<endl;
+                //x - (PX - 1) + px = 0 x+px =PX-1=1 x=0,px=1 || x=1,px=0
+                //所有gg[QC][px]的0位为0
                 fail[x - (PX - 1) + px] |= gg[QC][px];
+//                cout<<"x - (PX - 1) + px = "<<x - (PX - 1) + px<<endl;
+//                cout<<"fail[x - (PX - 1) + px] "<<printBs(fail[x - (PX - 1) + px])<<endl;
+//                cout<<endl;
+                int a=1;
             }
         }
     }
@@ -90,7 +127,7 @@ int main() {
     //遍历fail,如果没有fail即是命中
     for (int y = 0; y <= QY - PY; y++)
         for (int x = 0; x <= QX - PX; x++)
-            if (!fail[x][y]) {
+            if (!fail[x][y]) {//fail[0][0]=0
                 v.push_back({x, y});
             }
     cout << v.size() << endl;
