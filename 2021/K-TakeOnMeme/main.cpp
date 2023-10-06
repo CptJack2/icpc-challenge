d9b9da401161639b89c80c618852b8c0da2247fb
CptJack <844501812@qq.com>
2023-10-06 21:15:44 +0800

    2021 K

--- 2021/K-TakeOnMeme/main.cpp
+++ 2021/K-TakeOnMeme/main.cpp
@@ -1,19 +1,11 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <functional>
-#include <iostream>
-#include <tuple>
-#include <vector>
+#include "bits/stdc++.h"
 using namespace std;
 
-void Init() {
-    srand(time(0));
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-}
+//void Init() {
+//    srand(time(0));
+//    ios::sync_with_stdio(false);
+//    ::cin.tie(NULL);
+//}
 
 int64_t cmpx = 1, cmpy = 0;
 struct Point {
@@ -28,56 +20,83 @@ struct Point {
     int64_t lensqr() const { return x*x+y*y; }
 };
 
+vector<vector<int>> ch;
+vector<Point> p;
+int64_t ret = 0;
+
+pair<Point, Point> doit(int x) {
+    if (ch[x].size() == 0) return {p[x], p[x]};
+    auto[mntot, mxtot] = doit(ch[x][0]);
+    Point mndiff = mxtot + mntot, mxdiff = mndiff;
+    for (int i = 1; i < ch[x].size(); i++) {
+        auto[mn, mx] = doit(ch[x][i]);
+        mntot += mn;
+        mxtot += mx;
+        mndiff = min(mndiff, mx + mn);
+        mxdiff = max(mxdiff, mx + mn);
+    }
+    return {-mxtot + mndiff, -mntot + mxdiff};
+}
+
+pair<Point, Point> tryAngle(Point dir) {
+    cmpx = dir.x;
+    cmpy = dir.y;
+    function<pair<Point, Point>(int)> doit = [&](int x) -> pair<Point, Point> {
+        if (ch[x].size() == 0) return {p[x], p[x]};
+        auto[mntot, mxtot] = doit(ch[x][0]);
+        Point mndiff = mxtot + mntot, mxdiff = mndiff;
+        for (int i = 1; i < ch[x].size(); i++) {
+            auto[mn, mx] = doit(ch[x][i]);
+            mntot += mn;
+            mxtot += mx;
+            mndiff = min(mndiff, mx + mn);
+            mxdiff = max(mxdiff, mx + mn);
+        }
+        return {-mxtot + mndiff, -mntot + mxdiff};
+    };
+    auto[mn, mx] = doit(1);
+    ret = max(ret, mx.lensqr());
+    ret = max(ret, mn.lensqr());
+    return {mn, mx};
+}
+
+void traceHull(Point a, Point b) {
+    if (a == b) return;
+    auto[_, c] = tryAngle((b - a).ortho());
+    if (a < c) {
+        traceHull(a, c);
+        traceHull(c, b);
+    }
+}
+
+stringstream cin(R"(
+4
+3 2 3 4
+0 10 1
+0 3 6
+0 2 7
+)");
+
 int main() {
-    Init();
+//    Init();
 
     int N, M;
-    cin >> N;
-    vector<vector<int>> ch(N + 1);
-    vector<Point> p(N + 1);
+    ::cin >> N;
+    ch.resize(N + 1);
+    p.resize(N + 1);
     for (int i = 1; i <= N; i++) {
-        cin >> M;
+        ::cin >> M;
         if (M == 0) {
-            cin >> p[i].x >> p[i].y;
+            ::cin >> p[i].x >> p[i].y;
         } else {
             ch[i].resize(M);
-            for (auto &x : ch[i]) cin >> x;
+            for (auto &x : ch[i]) ::cin >> x;
         }
     }
 
-    int64_t ret = 0;
-    auto tryAngle = [&](Point dir) -> pair<Point, Point> {
-        cmpx = dir.x;
-        cmpy = dir.y;
-        function<pair<Point, Point>(int)> doit = [&](int x) -> pair<Point, Point> {
-            if (ch[x].size() == 0) return {p[x], p[x]};
-            auto[mntot, mxtot] = doit(ch[x][0]);
-            Point mndiff = mxtot + mntot, mxdiff = mndiff;
-            for (int i = 1; i < ch[x].size(); i++) {
-                auto[mn, mx] = doit(ch[x][i]);
-                mntot += mn;
-                mxtot += mx;
-                mndiff = min(mndiff, mx + mn);
-                mxdiff = max(mxdiff, mx + mn);
-            }
-            return {-mxtot + mndiff, -mntot + mxdiff};
-        };
-        auto[mn, mx] = doit(1);
-        ret = max(ret, mx.lensqr());
-        ret = max(ret, mn.lensqr());
-        return {mn, mx};
-    };
-    function<void(Point, Point)> traceHull = [&](Point a, Point b) {
-        if (a == b) return;
-        auto[_, c] = tryAngle((b - a).ortho());
-        if (a < c) {
-            traceHull(a, c);
-            traceHull(c, b);
-        }
-    };
     auto[left, right] = tryAngle({1, 0});
     traceHull(left, right);
     traceHull(right, left);
 
-    printf("%lld\n", ret);
+    cout<<ret<<endl;
 }
