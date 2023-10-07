f79e7e278998db4521ce89aec6a3edd4300abfef
CptJack <844501812@qq.com>
2023-10-07 18:08:47 +0800

    2021 C

--- 2021/C-FairDivision/main.cpp
+++ 2021/C-FairDivision/main.cpp
@@ -1,35 +1,36 @@
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <vector>
+#include "bits/stdc++.h"
+
 using namespace std;
 
-int main() {
-    int64_t N, M;
-    cin >> N >> M;
-
-    int p, q;
-    vector<double> pw(2);//所有整数的N次方
-    if (N > 60) goto fail;// 2^60>10e18, 当N>60, q<2， 不存在解
-
-    for (q = 2;; q++) {
-        pw.push_back(pow(q, N));
-        for (p = 1; p < q; p++) {
-            double d = pw[q] - pw[q - p];
-            if (d > 1.03 * M * q) {// mq/d > mp/d, 如果要每个海盗都能拿到整数, 需要mp/d>=1， 当mq/d <1 ，已经没可能有整数解
-                if (p == 1) goto fail;
-                continue;
-            }
-            __int128_t qp = 1, pp = 1;
-            for (int i = 0; i < N; i++) qp *= q;//q的N次方
-            for (int i = 0; i < N; i++) pp *= q - p;//q-p的N次方
-            if (__int128_t(M) * p % (qp - pp) == 0) goto done;
-        }
+int main(){
+    long long n,m;
+    cin>>n>>m;
+    // 2^60>10e18, 当N>60, q<2，不存在解
+    if(n>60) {
+        cout << "impossible" << endl;
+        return 0;
     }
 
-    done:
-    cout << p << ' ' << q << endl;
-    return 0;
-    fail:
-    cout << "impossible" << endl;
+    int p, q;
+    //brute force遍历可能的pq组合
+    for(q=2; ;++q)
+        for(p=1;p<q;++p){
+            //由 mq/(q^n-(q-p)^n) > mp/(q^n-(q-p)^n), 如果要每个海盗都能拿到整数, 需要mp/(q^n-(q-p)^n)>=1， 当mq/(q^n-(q-p)^n) <1 ，已经没可能有整数解
+            if(pow(q,n)-pow(q-p,n)>1.03*m*q){
+                if(p!=1)//当前q下找不到整数解了
+                    continue;
+                else{//如果p=1，再增大q也不可能有整数解了
+                    cout << "impossible" << endl;
+                    return 0;
+                }
+            }
+            __int128_t qn = 1, qpn = 1;//q和q-p的N次方，double不准确
+            for (int i = 0; i < n; i++) qn *= q;
+            for (int i = 0; i < n; i++) qpn *= q - p;
+            //找到合适的最小的p和q
+            if (__int128_t(m) * p % (qn - qpn) == 0) {
+                cout<<p<<' '<<q<<endl;
+                return 0;
+            }
+        }
 }
