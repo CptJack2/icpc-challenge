68eea236801ae36589443290f3ce2295b66794cb
CptJack <844501812@qq.com>
2023-10-18 23:33:20 +0800

    2021 G

--- 2021/G-MosaicBrowsing/main.cpp
+++ 2021/G-MosaicBrowsing/main.cpp
@@ -28,10 +28,6 @@ int main() {
             motifColorOrWild[cp - 1 - x].set(rp - 1 - y);
             motifColorLayers[color][cp - 1 - x].set(rp - 1 - y);
         }
-    //motif颜色图层反转
-    for (auto &v : motifColorLayers)
-        for (auto &b : v)
-            b = ~b;
     ::cin >> rq >> cq;
     vector<bs> fail(cq);
     //根据mosiac构造的待匹配,都按颜色分层
@@ -46,10 +42,13 @@ int main() {
                 if (fx > cq - cp) break;//motif是否超出mosiac右边界
                 if (colorMet[color][px] != y) {//这里每次比对motif的px列
                     colorMet[color][px] = y;
+                    //motif颜色图层反转
+                    auto rev=~motifColorLayers[color][px]&motifColorOrWild[px];
+                    //左右移,去匹配mosiac对应位置
                     if (y < rp) {//g[px] & cg[QC][px] 有色且色不对,&出来那个位置才是1, motif的px列,每一格是否match当前这个QC
-                        matchResult[color][px] = (motifColorOrWild[px] & motifColorLayers[color][px]) >> (rp - 1) - y;//左右移,去匹配mosiac对应位置
+                        matchResult[color][px]=rev>> (rp - 1) - y;
                     } else {
-                        matchResult[color][px] = (motifColorOrWild[px] & motifColorLayers[color][px]) << y - (rp - 1);
+                        matchResult[color][px]=rev<< y - (rp - 1);
                     }
                 }
                 fail[x - (cp - 1) + px] |= matchResult[color][px];
