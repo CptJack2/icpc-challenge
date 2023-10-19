a1ada4071d0a08f0d47a1e5cc0b946cff3c6a070
CptJack <844501812@qq.com>
2023-10-19 14:26:10 +0800

    2021 H

--- 2021/H-PrehistoricPrograms/verify.py
+++ 2021/H-PrehistoricPrograms/verify.py
@@ -3,30 +3,39 @@
 import subprocess
 import sys
 
-fn=sys.argv[1]
+# FN="secret-01"
 
-def execute_bash_command(command):
-    result = subprocess.run("bash -c \""+command+"\"", shell=True, capture_output=True, text=True)
-    if result.returncode == 0:
-        output = result.stdout
-        return output
+if len(sys.argv)>=2 or "FN" in globals():
+    if len(sys.argv)>=2:
+        fn=sys.argv[1]
+    elif "FN" in globals():
+        fn=eval("FN")
+    def execute_bash_command(command):
+        result = subprocess.run("bash -c \""+command+"\"", shell=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            output = result.stdout
+            return output
 
-out=execute_bash_command("cat data/"+fn+".in | ./main").strip()
-if out=="impossible":
-    ans=execute_bash_command("cat data/"+fn+".ans").strip()
-    if ans==out:
-        print("succ")
-    else:
-        print("failed")
-    exit()
+    out=execute_bash_command("cat data/"+fn+".in | ./main").strip()
+    if out=="impossible":
+        ans=execute_bash_command("cat data/"+fn+".ans").strip()
+        if ans==out:
+            print("succ")
+        else:
+            print("failed")
+        exit()
 
-strs=[]
-with open("data/"+fn+".in") as f:
-    n=int(f.readline())
-    for i in range(n):
-        strs.append(f.readline().strip())
+    strs=[]
+    with open("data/"+fn+".in") as f:
+        n=int(f.readline())
+        for i in range(n):
+            strs.append(f.readline().strip())
 
-indexs = [int(x) for x in out.split("\n") if x]
+    indexs = [int(x) for x in out.split("\n") if x]
+else:
+    strs=['((','(',')))((()','))']
+    indexs=[2,1,3,4]
+
 str=""
 for i in indexs:
     if i-1>=len(strs):
