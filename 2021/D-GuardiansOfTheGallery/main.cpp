f7dd5056397177263492eb03721a121d189cc628
CptJack <844501812@qq.com>
2023-11-19 19:40:07 +0800

    2021 D

--- 2021/D-GuardiansOfTheGallery/main.cpp
+++ 2021/D-GuardiansOfTheGallery/main.cpp
@@ -48,15 +48,17 @@ int main(){
     cin>>g.x>>g.y;
     cin>>s.x>>s.y;
     vector<Point> endpoints;
+    //找指向每个顶点方向距离最近的intersecting边
+    polygon.push_back(g);//g点方向也需要找
     for(auto p:polygon){
         vector<tuple<double,int>> intersections;//交点距离，遮挡面
+        if((p-s).Len()<EPS)continue;
         p=(p-s)/(p-s).Len()+s;
         //找出sculpture向各个顶点出发, 不被遮挡能走的最远距离maxd，即视野不被遮挡的区域的终点(在多边形的边上)
         for(int i=0;i<n;++i){
-            auto& a=polygon[i], &b=polygon[i!=n?i+1:0];
-            int side;
-            auto d= RayIntersect(s,p,a,b,&side);
-            if(d>EPS)
+            int side=0;
+            auto d= RayIntersect(s,p,polygon[i],polygon[(i+1)%n],&side);
+            if(d>-EPS)
                 intersections.emplace_back(d,side);
         }
         ranges::sort(intersections);
@@ -67,10 +69,9 @@ int main(){
             sides|=s;
             if(sides==3)break;
         }
-        endpoints.push_back(s+(p-s)*maxd);
+        endpoints.push_back(s+(p-s)*maxd);//从sculpture出发,沿着射线,视野不被遮挡的区域的终点(在多边形的边上)
         //计算顶点p2，到sculpture发出的穿过顶点i的射线，的距离,以及交点
         for(auto p2:polygon){
-            if((p-p2).Len()<EPS)continue;
             auto ortho=Point(s.y-p.y,p.x-s.x)*1e5;
             auto d= RayIntersect(s,p,p2,p2+ortho);
             if(d<-EPS)
@@ -79,6 +80,7 @@ int main(){
                 endpoints.push_back(s+(p-s)*d);
         }
     }
+    polygon.pop_back();
     //dijkstra算法求最短长度
     vector<Point> points(polygon.size()+2+endpoints.size());
     int i=0;
@@ -89,12 +91,14 @@ int main(){
     priority_queue<pair<double,int>, vector<pair<double,int>>, greater<>> que;
     vector<double> dist(points.size(),1e20);//从g到每个节点的最短路径长度
     que.emplace(0.0,0);//从0号guard的起始位置出发
-    for(i=que.top().second;i<n;i=que.top().second){
+    for(i=que.top().second;i<=n;i=que.top().second){
         auto d=que.top().first;
+        assert(que.size());
         que.pop();
         if(d>=dist[i])continue;
         dist[i]=d;
         for(int j=0;j<points.size();++j){
+            if(i==j)continue;
             auto p1=points[i], p2=points[j];
             auto ln=(p1-p2).Len();
             if(ln<EPS ||
@@ -102,23 +106,28 @@ int main(){
                i>=1&&i<=n && PointOnLine(polygon[i-1], polygon[(i) % n], p2) ||
                i>=1&&i<=n && PointOnLine(polygon[i-1], polygon[(i + n - 2) % n], p2)) {
                 que.emplace(d+ln,j);
-                break;
+                continue;
             }
             //判断p1p2是否被poly完全包含
-            p2=p1+(p2-p1)/(p2-p1).Len();
+            bool fullContained=true;
+            p2=p1+(p2-p1)/ln;
             for(int k=0;k<polygon.size();++k){
                 auto rd= RayIntersect(p1,p2,polygon[k],polygon[(k+1)%n]);
-                if(rd>EPS && rd<ln-EPS)break;
+                if(rd>EPS && rd<ln-EPS) {
+                    fullContained=false;
+                    break;
+                }
             }
+            if(!fullContained)continue;
             //判断一下p1p2上任意一点是不是在poly内部，来保证p1p2全段都在poly内部。通过p1p2中点画一任意射线, 看穿过poly为奇数还是偶数次
+            int cnt=0;
             p1 = p1*2/3 + p2/ 3;
             p2 = p1 + Point(cos(10), sin(10));
-            int cnt=0;
             for(int k=0;k<n;++k){
                 auto rd= RayIntersect(p1,p2,polygon[k],polygon[(k+1)%n]);
                 cnt+=(rd>EPS);
             }
-            if(cnt%2==0)
+            if(cnt%2==1)
                 que.emplace(d+ln,j);
         }
     }
