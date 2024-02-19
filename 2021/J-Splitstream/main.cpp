400de69a2079f1578ef4632b69a5a071968736fa
xilong.chen <xilong.chen@kiwigame.com>
2024-02-19 18:46:32 +0800

    2021 J no pointer

--- 2021/J-Splitstream/main.cpp
+++ 2021/J-Splitstream/main.cpp
@@ -8,10 +8,10 @@ struct Node{
     int input2;
     int output1;
     int output2;
-    Node* inode1;
-    Node* inode2;
-    Node* onode1;
-    Node* onode2;
+    int inode1;
+    int inode2;
+    int onode1;
+    int onode2;
 };
 
 int n,m,q;
@@ -19,10 +19,10 @@ int n,m,q;
 int main(){
     cin>>m>>n>>q;
     //n个node最多有2*n个流
-    vector<Node*> inputToNode(2*n+2);
-    vector<Node*> outputToNode(2*n+2);
-    vector<Node> nodes(n);
-    for(int i=0;i<n;++i){
+    vector<int> inputToNode(2*n+2);
+    vector<int> outputToNode(2*n+2);
+    vector<Node> nodes(n+1);
+    for(int i=1;i<=n;++i){
         char type;
         int x,y,z;
         cin>>type>>x>>y>>z;
@@ -30,23 +30,23 @@ int main(){
         Node& node= nodes[i];
         node.type=type;
         if(type=='S'){
-            inputToNode[x] =&node;
-            outputToNode[y]=&node;
-            outputToNode[z]=&node;
+            inputToNode[x] =i;
+            outputToNode[y]=i;
+            outputToNode[z]=i;
             node.input1=x;
             node.output1=y;
             node.output2=z;
         }else{
-            inputToNode[x] =&node;
-            inputToNode[y] =&node;
-            outputToNode[z]=&node;
+            inputToNode[x] =i;
+            inputToNode[y] =i;
+            outputToNode[z]=i;
             node.input1=x;
             node.input2=y;
             node.output1=z;
         }
     }
     //将各个node连接成树
-    for(int i=0;i<n;++i) {
+    for(int i=1;i<=n;++i) {
         Node& node= nodes[i];
         node.inode1=outputToNode[node.input1];
         node.inode2=outputToNode[node.input2];
@@ -56,8 +56,9 @@ int main(){
     //通过一次dfs计算每个stream的size
     vector<int> streamSize(2*n+2);
     streamSize[1]=m;
-    function<void(Node*)> dfsCalcStreamSize=[&](Node* p){
-        if(!p)return;
+    function<void(int)> dfsCalcStreamSize=[&](int nodeIndex){
+        if(!nodeIndex)return;
+        auto p=&nodes[nodeIndex];
         if(p->type=='S'){
             streamSize[p->output1]=streamSize[p->input1]/2+streamSize[p->input1]%2;
             streamSize[p->output2]=streamSize[p->input1]/2;
@@ -67,8 +68,8 @@ int main(){
         dfsCalcStreamSize(p->onode1);
         dfsCalcStreamSize(p->onode2);
     };
-    Node* root=inputToNode[1];
-    dfsCalcStreamSize(root);
+//    Node* root=inputToNode[1];
+    dfsCalcStreamSize(inputToNode[1]);
     //读入查询, 通过在树上回溯,计算出原来的编号
     for(int i=0;i<q;++i){
         int x,k;
@@ -78,24 +79,24 @@ int main(){
             continue;
         }
         //沿着树回溯
-        auto node=outputToNode[x];
-        while(node){
-            if(node->type=='S'){
+        auto nodeIndex=outputToNode[x];
+        while(nodeIndex){
+            if(nodes[nodeIndex].type=='S'){
                 //如果ostream x是node的o2, 则k是对应的istream 1的第2*k个元素；如果是o1，则是第2*k-1个
-                k=2*k-(x==node->output1);
-                x=node->input1;
-                node=node->inode1;
+                k=2*k-(x==nodes[nodeIndex].output1);
+                x=nodes[nodeIndex].input1;
+                nodeIndex=nodes[nodeIndex].inode1;
             }else{
-                int sz=min(streamSize[node->input1],streamSize[node->input2]);
+                int sz=min(streamSize[nodes[nodeIndex].input1],streamSize[nodes[nodeIndex].input2]);
                 if(k<=2*sz){
                     //对于M node，ostream x的第k个，对应于i1的第k/2+1（k为奇数），或i2的第k/2个（k为偶数）
-                    x=k%2==0?node->input2:node->input1;
-                    node=k%2==0?node->inode2:node->inode1;
+                    x=k%2==0?nodes[nodeIndex].input2:nodes[nodeIndex].input1;
+                    nodeIndex=k%2==0?nodes[nodeIndex].inode2:nodes[nodeIndex].inode1;
                     k=(k+1)/2;
                 }else{
                     //
-                    x=streamSize[node->input1]>streamSize[node->input2]?node->input1:node->input2;
-                    node=streamSize[node->input1]>streamSize[node->input2]?node->inode1:node->inode2;
+                    x=streamSize[nodes[nodeIndex].input1]>streamSize[nodes[nodeIndex].input2]?nodes[nodeIndex].input1:nodes[nodeIndex].input2;
+                    nodeIndex=streamSize[nodes[nodeIndex].input1]>streamSize[nodes[nodeIndex].input2]?nodes[nodeIndex].inode1:nodes[nodeIndex].inode2;
                     k=k-sz;
                 }
 
