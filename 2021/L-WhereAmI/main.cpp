76d1f073738b02aeb3af1c44868062fab7496d69
CptJack <leninchen03@gmail.com>
2024-05-23 19:40:11 +0800

    2021 L

--- 2021/L-WhereAmI/main.cpp
+++ 2021/L-WhereAmI/main.cpp
@@ -15,13 +15,16 @@ int main(){
     }
     //生成螺旋距离矩阵
     vector<vector<int>> distance(201,vector<int>(201));
-    for(int x=100,y=100,dx=0,dy=1,step=0,stepn=1,cur=0 ; x>=0&&x<=201&&y>=0&&y<=201 ; x+=dx,y+=dy,++cur){
+    for(int x=100,y=100,dx=0,dy=1,step=0,stepn=1,cur=0 ; x>=0&&x<201&&y>=0&&y<201 ; ++cur){
         distance[y][x]=cur;
+        x+=dx;
+        y+=dy;
         ++step;
         if(step==stepn){
-            step = 0;
+            //切换方向
             swap(dx, dy);
             dy = -dy;
+            step = 0;
             if(dy!=0)
                 ++stepn;
         }
@@ -37,22 +40,25 @@ int main(){
                 gridDistances[dist].push_back(x2+(y2-1)*dx);
             }
     //根据距离编码区分格子
-    vector<int> gridCategory(dx*dy, 0), //每一个格子属于的类别，动态更新
-        gridCategorySteps(dx*dy, 0), //每一类格子的step数量
+    vector<int> gridCategory(dx*dy+1, 0), //每一个格子属于的类别，动态更新
+        categorySteps(dx * dy, 0), //每一类格子的step数量
         categorySize = {dx*dy}; //每个类别的格子数
     for(int dist=0;categorySize.size()<dx*dy;++dist){
         auto& gridAtDist=gridDistances[dist];
         //按照category顺序排序
         sort(gridAtDist.begin(), gridAtDist.end(), [&](int a, int b){return gridCategory[a] < gridCategory[b];});
         for(int i=0,j=0; i < gridAtDist.size(); i=j){
-            for(; j < gridAtDist.size() && gridCategory[j] == gridCategory[i]; ++j);
+            for(; j < gridAtDist.size() && gridCategory[gridAtDist[j]] == gridCategory[gridAtDist[i]]; ++j);
             int& sz=categorySize[gridCategory[gridAtDist[i]]];
-            if (j - i == sz) continue;//当前comp category的所有格子编码都是一样的,靠这个X无法区分
+            //当前comp category的所有格子编码都是一样的,靠这个X无法区分
+            if (j - i == sz)
+                continue;
+            //当前X可以唯一区分一个格子，新建一类
             if (j - i == 1)
-                gridCategorySteps[categorySize.size()] = dist;//当前X可以唯一区分一个格子，新建一类
+                categorySteps[categorySize.size()] = dist;
             sz-=j-i;
             if (sz == 1)
-                gridCategorySteps[gridCategory[gridAtDist[i]]] = dist;//当前X的位置是.的格子只有一个,它可以被区分
+                categorySteps[gridCategory[gridAtDist[i]]] = dist;//当前X的位置是.的格子只有一个,它可以被区分
             //当前距离的category分裂开来，继续流程
             for (int k = i; k < j; k++) gridCategory[gridAtDist[k]] = categorySize.size();
             categorySize.push_back(j - i);
@@ -61,15 +67,15 @@ int main(){
     //统计数据
     int mx = 0, tot = 0;
     for (int i = 0; i < dx * dy; i++) {
-        mx = max(mx, gridCategorySteps[i]);
-        tot += gridCategorySteps[i];
+        mx = max(mx, categorySteps[i]);
+        tot += categorySteps[i];
     }
 
     cout << fixed << setprecision(9) << double(tot) / dx / dy << endl;
     cout << mx << endl;
     bool first = true;
     for (int i = 1; i <= dx * dy; i++)
-        if (gridCategorySteps[gridCategory[i]] == mx) {
+        if (categorySteps[gridCategory[i]] == mx) {
             if (!first) cout << ' ';
             first = false;
             cout << '(' << (i-1) % dx + 1 << ',' << (i-1) / dx + 1 << ')';
